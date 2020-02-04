fbf54c68aff7744dc85fdbe14f50164d430c4bcc
PavanPrasad473 <spavanprasad@gmail.com>
2020-02-05 00:41:36 +0530

    initial commit

---
+++ VersioningDB/dbo/Stored Procedures/spAddVideo.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[spAddVideo]
+(
+	@Name varchar(255),
+	@ReleaseDate datetime,
+	@Genre varchar(255),
+	@Classification TINYINT
+)
+AS
+
+	DECLARE @GenreId tinyint
+	SET @GenreId = (SELECT Id FROM Genres WHERE Name = @Genre)
+
+	INSERT INTO Videos (Name, ReleaseDate,GenreId,Classification)
+	VALUES (@Name, @ReleaseDate,@GenreId,@Classification)
+
+	select 1
+	select 2
+
+	
+
---
+++ VersioningDB/dbo/Tables/Videos.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Videos] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [Name]           VARCHAR (255) NOT NULL,
+    [ReleaseDate]    DATETIME      NOT NULL,
+    [GenreId]        TINYINT       NOT NULL,
+    [Classification] TINYINT       DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_Videos] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_GenreId] FOREIGN KEY ([GenreId]) REFERENCES [dbo].[Genres] ([Id])
+);
+
