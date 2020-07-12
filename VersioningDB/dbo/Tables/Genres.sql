8e14816dc2fe05d1c0b71ef09f230ca5de061308
PavanPrasad473 <spavanprasad@gmail.com>
2020-07-13 01:40:44 +0530

    COMMIT

--- VersioningDB/dbo/Tables/Genres.sql
+++ VersioningDB/dbo/Tables/Genres.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Genres] (
-    [Id]   TINYINT       NOT NULL,
-    [Name] VARCHAR (255) NOT NULL,
+    [Id]          TINYINT       NOT NULL,
+    [Name]        VARCHAR (255) NOT NULL,
+    [CreatedTime] DATETIME      DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_Genres] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
